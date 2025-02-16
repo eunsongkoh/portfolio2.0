6068338d4809a02df16ca1b2273d598ab8b98d40
eunsongkoh <eunsongkohh@gmail.com>
2025-02-16 00:55:16 -0800

    updated videos

--- src/components/Multimedia/Videos.tsx
+++ src/components/Multimedia/Videos.tsx
@@ -3,7 +3,7 @@ import { YouTubeEmbed } from "react-social-media-embed";
 export default function Videos() {
   return (
     <div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 lg:gap-8 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 lg:gap-8 gap-4 m-1">
         <InstagramEmbed
           url="https://www.instagram.com/reel/C_jBa3uSxco/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
           width={328}
